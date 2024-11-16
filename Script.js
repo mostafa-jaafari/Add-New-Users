e67e4b01844b76923d1e7754806838cf6f6eb4d6
Mostafa Jaafari <jaafarimostafa081@gmail.com>
2024-11-16 22:40:16 +0100

    Add requirement of Submit and change the logo icon

--- Script.js
+++ Script.js
@@ -43,25 +43,29 @@ function ClearData(){
 }
 // ------------ Read Data OnClick ------------
 CreateButton.onclick = function() {
-    Create_User();
-    Read_Data();
+    if(Name.value !== '' && Age.value !== '' && Occupation.value !== '' && Location.value !== ''){
+        Create_User();
+        Read_Data();
+    }else{
+        return;
+    }
 };
 // ------------ Read Data Table ------------
 function Read_Data(){
     let table = '';
-    for(let i = 0; i < UsersData.length; i++){
-        table += `
-            <tr class="border-b border-slate-700 text-white hover:bg-slate-900">
-                    <td class="px-6 py-4 text-sm">${UsersData[i].Name}</td>
-                    <td class="px-6 py-4 text-sm">${UsersData[i].Age}</td>
-                    <td class="px-6 py-4 text-sm">${UsersData[i].Occupation}</td>
-                    <td class="px-6 py-4 text-sm">${UsersData[i].Location}</td>
-                    <td class="px-6 py-4 m-auto text-sm font-semibold text-red-500"><i onclick="Delete_User(${i})" class="fa-solid fa-trash cursor-pointer hover:bg-red-100 rounded p-1"></i></td>
-                    <td class="px-6 py-4 m-auto text-sm font-semibold text-blue-500"><i onclick="Update_User(${i})" class="fa-solid fa-pen-to-square hover:bg-blue-100 p-1 rounded cursor-pointer"></i></td>
-                </tr>
-        `;
-    }
-    document.getElementById("Tbody").innerHTML = table;
+        for(let i = 0; i < UsersData.length; i++){
+            table += `
+                <tr class="border-b border-slate-700 text-white hover:bg-slate-900">
+                        <td class="px-6 py-4 text-sm">${UsersData[i].Name}</td>
+                        <td class="px-6 py-4 text-sm">${UsersData[i].Age}</td>
+                        <td class="px-6 py-4 text-sm">${UsersData[i].Occupation}</td>
+                        <td class="px-6 py-4 text-sm">${UsersData[i].Location}</td>
+                        <td class="px-6 py-4 m-auto text-sm font-semibold text-red-500"><i onclick="Delete_User(${i})" class="fa-solid fa-trash cursor-pointer hover:bg-red-100 rounded p-1"></i></td>
+                        <td class="px-6 py-4 m-auto text-sm font-semibold text-blue-500"><i onclick="Update_User(${i})" class="fa-solid fa-pen-to-square hover:bg-blue-100 p-1 rounded cursor-pointer"></i></td>
+                    </tr>
+            `;
+        }
+        document.getElementById("Tbody").innerHTML = table;
 
     if (UsersData.length > 0) {
         DeleteAll.innerHTML = `<button onclick="DeleteAllUsers()" class="fixed bottom-3 text-red-500 w-10 h-8 transition-all duration-500 ease-in-out hover:w-32 overflow-hidden cursor-pointer rounded font-semibold bg-red-200 py-1 px-2 shadow-md shadow-black-900"><i class="fa-solid fa-trash"></i> Delete All ${UsersData.length}</span></button>
